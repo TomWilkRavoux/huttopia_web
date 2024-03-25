fe0dddafcf667ee14c73fa6b57e1b5e5e24f7ea1
TomWilkRavoux <tomwilkwilk@gmail.com>
2024-03-25 17:53:23 +0100

    fix

--- web/src/App.js
+++ web/src/App.js
@@ -1,20 +1,33 @@
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import {Life} from './Pages/Life' 
 import { Home } from './Pages/Home.js'
 import './App.css';
+import React, { useEffect, useState } from 'react';
+
+const [backendData, setBackendData] = useState([]);
+
+  useEffect(() => {
+    fetch("/api")
+      .then(response => response.json())
+      .then(data => setBackendData(data))
+      .catch(error => console.error('Erreur de récupération des données depuis le backend:', error));
+  }, []);
+
 
 export default function App() {
   return(
+  
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/life" element={<Life />}/>
-        </Routes>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/life" element={<Life />}/>
+          </Routes>
       </BrowserRouter>
-    </div>
+    </div>  
+    
   )
 }
-
 
