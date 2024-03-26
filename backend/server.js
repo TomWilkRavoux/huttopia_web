7f299227110047e888f92f71d6213ac5504f52a0
florian thubin <florian.thubin@gmail.com>
2024-03-26 10:22:27 +0100

    ddd

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-const cors = require('cors');
 
 const mysql = require('mysql');
 const express = require('express');
@@ -7,55 +6,21 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 5000;
 
-// Configuration de la connexion à la base de données
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '', // Ajoutez votre mot de passe si nécessaire
-  database: 'huttopia'
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : ''
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.error('Erreur lors de la connexion à la base de données MySQL :', err);
-    return;
-  }
-  console.log('Connecté à la base de données MySQL !');
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+  console.log('The solution is: ', rows[0].solution);
 });
 
-app.use(bodyParser.json());
-
-// Route pour gérer la connexion
-app.post('/api/login', (req, res) => {
-  const { email, password } = req.body;
-  
-  // Requête SQL pour récupérer l'utilisateur avec l'email spécifié
-  const query = 'SELECT * FROM admin WHERE email = ?';
-  
-  connection.query(query, [email], (err, results) => {
-    if (err) {
-      console.error('Erreur lors de l\'interrogation de la base de données :', err);
-      res.status(500).json({ message: 'Erreur serveur lors de la connexion.' });
-      return;
-    }
-
-    if (results.length === 0) {
-      // Aucun utilisateur trouvé avec cet email
-      res.status(401).json({ message: 'Nom d\'utilisateur ou mot de passe incorrect.' });
-      return;
-    }
-
-    const user = results[0];
-    if (user.password !== password) {
-      // Mot de passe incorrect
-      res.status(401).json({ message: 'Nom d\'utilisateur ou mot de passe incorrect.' });
-      return;
-    }
-
-    // Authentification réussie
-    res.status(200).json({ message: 'Connexion réussie ! Redirection vers la page de tableau de bord...' });
-  });
-});
+connection.end();
 
 
 
