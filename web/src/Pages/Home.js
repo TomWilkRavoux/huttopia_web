54b73843b6b6b981e378e7046a2b41d6affb70e5
TomWilkRavoux <tomwilkwilk@gmail.com>
2024-03-28 22:12:54 +0100

    debut du chill

--- web/src/Pages/Home.js
+++ web/src/Pages/Home.js
@@ -1,17 +1,20 @@
+import HuttopiaLogo from '../asset/img/Huttopia_logo.png';
 export function Home(){
     return(
-        <div>
-            <body className="bg-[#ECF1DD] min-h-screen">
-                <header>
+        <div className="bg-[#ECF1DD] min-h-screen flex flex-col items-center">
+            <header className="text-center font-family-MaPolice text-4xl">
+                <h1>Huttopia Bourg-Saint-Maurice</h1>
+            </header>
+            <main className="flex justify-center items-center flex-grow">
+                <div style={{ width: '25em', height: 'auto' }} >
+                    <a href="/HomeTwo">
+                        <img src={HuttopiaLogo} alt="coucou" className="w-full h-auto" />
+                    </a>
+                </div>
+            </main>
+            <footer>
 
-                </header>
-                <main>
-                    <h1 className="text-red-700">CONNARD</h1>
-                </main>
-                <footer>
-
-                </footer>
-            </body>
+            </footer>
         </div>
     )
 }
