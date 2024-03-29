23f3b54a94550eaad7728ffadce6f6945b64836e
TomWilkRavoux <tomwilkwilk@gmail.com>
2024-03-29 12:04:32 +0100

    dashcommande css

--- web/src/Pages/Dashcommande.js
+++ web/src/Pages/Dashcommande.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
-import { Addcommande } from "../Components/Addcommande";
 import Logout from '../Components/logout'
 
 
@@ -58,72 +57,73 @@ export function Dashcommande() {
 
   return (
 
-    <div className="table-auto">
+    <div className="table-auto bg-[#ECF1DD] min-h-screen">
       <div>
         <Logout />
       </div>
       <h1 className="text-2xl font-bold mb-4 text-center">Liste des commandes</h1>
-      <table className="w-full border-collapse border border-gray-200">
+      <table className="w-full border-collapse custom-border">
         <thead>
-          <tr className="bg-gray-100">
-            <th className="border border-gray-200 px-4 py-2">Nom <br /> Emplacement <br /> Téléphone </th>
-            <th className="border border-gray-200 px-4 py-2">Baguette tradition</th>
-            <th className="border border-gray-200 px-4 py-2">Pain sans gluten</th>
-            <th className="border border-gray-200 px-4 py-2">Pain complet</th>
-            <th className="border border-gray-200 px-4 py-2">Pain aux céréales</th>
-            <th className="border border-gray-200 px-4 py-2">Croissant</th>
-            <th className="border border-gray-200 px-4 py-2">Pain au chocolat</th>
-            <th className="border border-gray-200 px-4 py-2">Chaussons aux pommes</th>
-            <th className="border border-gray-200 px-4 py-2">Quantité</th>
-            <th className="border border-gray-200 px-4 py-2">Actions</th>
+          <tr className="custom-border">
+            <th className="custom-border bg-[#00533C] text-w px-4 py-2">Nom <br /> Emplacement <br /> Téléphone </th>
+            <th className="custom-border bg-[#00533C] text-w px-4 py-2">Baguette tradition</th>
+            <th className="custom-border bg-[#00533C] text-w px-4 py-2">Pain sans gluten</th>
+            <th className="custom-border bg-[#00533C] text-w px-4 py-2">Pain complet</th>
+            <th className="custom-border bg-[#00533C] text-w px-4 py-2">Pain aux céréales</th>
+            <th className="custom-border bg-[#00533C] text-w px-4 py-2">Croissant</th>
+            <th className="custom-border bg-[#00533C] text-w px-4 py-2">Pain au chocolat</th>
+            <th className="custom-border bg-[#00533C] text-w px-4 py-2">Chaussons aux pommes</th>
+            <th className="custom-border bg-[#00533C] text-w px-4 py-2">Quantité</th>
+            <th className="custom-border bg-[#00533C] text-w px-4 py-2">Actions</th>
             
           </tr>
         </thead>
         <tbody>
           {data.map((da, index) => (
-            <tr key={`${da.nom}-${da.emplacement}-${da.telephone}`} className="border border-gray-200">
-              <td className="border border-gray-200 px-4 py-2 text-center">
+            <tr key={`${da.nom}-${da.emplacement}-${da.telephone}`} className="custom-border">
+              <td className="custom-border px-4 py-2 text-center">
                 {da.nom} <br /> {da.emplacement} <br /> {da.telephone}
               </td>
-              <td className="border border-gray-200 px-4 py-2 text-center">
+              <td className="custom-border px-4 py-2 text-center">
               {da.commandes.find(commande => commande.nom_article === 'Baguette tradition')?.quantite_commande || ''}
             </td>
-            <td className="border border-gray-200 px-4 py-2 text-center">
+            <td className="custom-border px-4 py-2 text-center">
               {da.commandes.find(commande => commande.nom_article === 'Pain sans gluten')?.quantite_commande || ''}
             </td>
-            <td className="border border-gray-200 px-4 py-2 text-center">
+            <td className="custom-border px-4 py-2 text-center">
               {da.commandes.find(commande => commande.nom_article === 'Pain complet')?.quantite_commande || ''}
             </td>
-            <td className="border border-gray-200 px-4 py-2 text-center">
+            <td className="custom-border px-4 py-2 text-center">
               {da.commandes.find(commande => commande.nom_article === 'Pain aux céréale')?.quantite_commande || ''}
             </td>
-            <td className="border border-gray-200 px-4 py-2 text-center">
+            <td className="custom-border px-4 py-2 text-center">
               {da.commandes.find(commande => commande.nom_article === 'Croissant')?.quantite_commande || ''}
             </td>
-            <td className="border border-gray-200 px-4 py-2 text-center">
+            <td className="custom-border px-4 py-2 text-center">
               {da.commandes.find(commande => commande.nom_article === 'Pain au chocolat')?.quantite_commande || ''}
             </td>
-            <td className="border border-gray-200 px-4 py-2 text-center">
+            <td className="custom-border px-4 py-2 text-center">
               {da.commandes.find(commande => commande.nom_article === 'Chausson aux pommes')?.quantite_commande || ''}
             </td>
 
-              <td className="border border-gray-200 px-4 py-2 text-center">
-                
+              <td className="custom-border px-4 py-2 text-center">
+                {da.commandes.reduce((acc, curr) => acc + curr.quantite_commande, 0)}
               </td>
-              <td className="border border-gray-200 px-4 py-2 flex items-center justify-center">
-                  <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mr-2" onClick={() => handleDelete(da.id)}>Supprimer</button>
+              <td className="px-4 py-2 flex items-center justify-center">
+                  <button className="border bg-[#ff0000] hover:bg-[#8b0000] transition-colors duration-300 text-w font-bold py-2 px-4 rounded mr-2" onClick={() => handleDelete(da.id)}>Supprimer</button>
+                {/* 
                 <Link to={`/update/${da.id}`}>
-                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Modifier</button>
+                  <button className="border bg-[#0000ff] hover:bg-[#00008b] transition-colors duration-300 text-w font-bold py-2 px-4 rounded">Modifier</button>
                 </Link>
+                */}
               </td>
             </tr>
           ))}
         </tbody>
       </table>
-      <Addcommande/>
       
-      <div className="mt-4 flex justify-center">
-        <Link to="/adduser" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Ajouter une commande</Link>
+      <div className="mt-4 flex justify-center"> 
+        {/* <Link to="/adduser" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Ajouter une commande</Link> */}
       </div>
     </div>
   
